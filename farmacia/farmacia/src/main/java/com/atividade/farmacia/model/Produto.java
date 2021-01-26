6f7c20824a223473b2852255f295c95cb955872b
jorgehauck <jorge.hauck1997@outlook.com>
2021-01-26 16:03:07 -0300

    Atividade CRUD Farmácia Spring Boot

---
+++ farmacia/farmacia/src/main/java/com/atividade/farmacia/model/Produto.java
@@ -0,0 +1,66 @@
+package com.atividade.farmacia.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 4, max = 20)
+	private String nome;
+	
+	@NotNull
+	@Digits(integer = 5, fraction = 2)
+	private BigDecimal preco;
+	
+	@NotNull
+	private int qtdeEstoque;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getQtdeEstoque() {
+		return qtdeEstoque;
+	}
+
+	public void setQtdeEstoque(int qtdeEstoque) {
+		this.qtdeEstoque = qtdeEstoque;
+	}
+	
+	
+}
